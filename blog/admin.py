3177b91b3968fd72eadaadb0591319e5bad6b2f6
kovoli <kovoli1985@gmail.com>
2018-01-29 20:12:02 +0100

    добавил админку

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    # поля модели, которые будут отображаться на странице списка
+    list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    # поле «slug» заполняется автоматически
+    # raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
+
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -5,13 +5,13 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     STATUS_CHOICES = (
-        ('draft', 'DRAFT'),
+        ('draft', 'Draft'),
         ('published', 'Published'),
     )
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, related_name='blog_posts')
-    body = models.TextField
+    body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -19,6 +19,7 @@ class Post(models.Model):
 
     class Meta:
         ordering = ('-publish',)
+
 
     def __str__(self):
         return self.title
