aa7cf7621f6666e6dc1c130f81dc7ccaf132d4b2
kovoli <kovoli1985@gmail.com>
2018-01-31 18:46:23 +0100

    добавил пагинацию

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 
 def post_list(request):
-    posts = Post.objects.filter(status='published')
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    object_list = Post.objects.all()
+    paginator = Paginator(object_list, 2)
+    page = request.GET.get('page')
+    try:
+        posts =paginator.page(page)
+    except PageNotAnInteger:
+        # Если страница не является целым числом, доставьте первую страницу
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Если страница за пределами допустимого диапазона доставляет последнюю страницу результатов
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
 
 
 def post_detail(request, post):
